c59368e661d625a4f1c329d48d13a969d2ec5564
Jacob <jacob.krueckl@gmail.com>
2021-02-01 21:50:26 -0700

    Working on about / linking to other pages.

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/about.html", function(req, res) {
+    res.sendFile(__dirname + "/about.html");
+});
+
 app.listen(port, function(){
     console.log("Server started and listening on port: " + port);
 });
