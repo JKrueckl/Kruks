f282376c0a230b349f192eab38c0a043c7a9a39a
Jacob <jacob.krueckl@gmail.com>
2021-02-01 17:40:04 -0700

    Fixing background image issue

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.get("/", function(req, res) {
 });
 
 app.listen(port, function(){
-    console.log("Server started and listening on port: 3000");
+    console.log("Server started and listening on port: " + port);
 });
